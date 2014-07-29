068a2d907777a95dca2863b4b1ae91ae002c12a1
InkSlinger666 <kengreenwood420666@gmail.com>
2014-07-29 11:29:40 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo broken_d2lte-userdebug
+add_lunch_combo broken_exhilarate-userdebug
 add_lunch_combo broken_flo-userdebug
 add_lunch_combo broken_hammerhead-userdebug
 add_lunch_combo broken_lt01wifi-userdebug
