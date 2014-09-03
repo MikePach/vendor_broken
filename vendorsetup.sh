39272b12bf7916517cf8656bd594a13068ebd12e
Randy DuCharme <radio.ad5gb@gmail.com>
2014-09-02 20:15:57 -0500

    Update vendorsetup.sh
    Adding a non-caf test device for Nexus 5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,3 +14,4 @@ add_lunch_combo broken_skyrocket-userdebug
 add_lunch_combo broken_t769-userdebug
 add_lunch_combo broken_toro-userdebug
 add_lunch_combo broken_wickedhh-userdebug
+add_lunch_combo broken_wickedn5-userdebug
