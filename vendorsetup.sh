4cb2a6c9b48052bf3d0ffce766488f304f8717e7
Randy DuCharme <radio.ad5gb@gmail.com>
2014-09-06 14:12:54 -0500

    Update vendorsetup.sh
    Added experimental G3 lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo broken_d2lte-userdebug
+add_lunch_combo broken_d851-userdebug
 add_lunch_combo broken_dlx-userdebug
 add_lunch_combo broken_flo-userdebug
 add_lunch_combo broken_grouper-userdebug
