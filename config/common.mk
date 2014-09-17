HyperionROM/vendor_hyperion

Add Theme Manager Support

config/common.mk
@@ -101,6 +101,9 @@ PRODUCT_PACKAGES += \
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
 
+# Theme Manager
+include vendor/hyperion/config/themes.mk
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/hyperion/overlay/common
 
 # Boot animation include

MikePach/vendor_broken

Conflicts:
	config/common.mk

config/common.mk
@@ -170,7 +170,11 @@ PRODUCT_PACKAGES += \
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
 
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/broken/overlay/common
+
+# Theme Manager
+include vendor/broken/config/themes.mk
 
 # Boot animation include
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))